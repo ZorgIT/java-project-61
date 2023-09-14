10480c838814909b1317577bb35824a07937d2a2
Zorgit <piligrimexe@gmail.com>
2023-09-14 22:50:03 +0300

    fix bug. Scanner have no input

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -7,8 +7,12 @@ public class Cli {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
-        var username = scanner.next();
-        System.out.println("Hello, " + username);
+        if (scanner.hasNext()) {
+            var username = scanner.next();
+            System.out.println("Hello, " + username);
+        } else {
+            System.out.println("No input provided!");
+        }
         System.exit(0);
     }
 }
