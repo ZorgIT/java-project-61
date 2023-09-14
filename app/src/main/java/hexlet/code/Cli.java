d45d2385d67e7f04b99738b22ac25b73f474f930
Zorgit <piligrimexe@gmail.com>
2023-09-14 21:11:36 +0300

    St3. Add Cli class with basic code

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void start() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name? ");
+        Scanner scanner = new Scanner(System.in);
+        var username = scanner.next();
+        System.out.println("Hello, " + username);
+        System.exit(0);
+    }
+}
