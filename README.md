fe636870d2d6d3930ae22f8c3e7baaa856a68cc0
Zorgit <piligrimexe@gmail.com>
2023-09-15 20:16:53 +0300

    St4. Add CodeClimate badge in README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ZorgIT/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/ZorgIT/java-project-61/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/fd2a1e010c133e2687ed/maintainability)](https://codeclimate.com/github/ZorgIT/java-project-61/maintainability)
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -7,12 +7,14 @@ public class Cli {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
-        if (scanner.hasNext()) {
-            var username = scanner.next();
+        if (scanner.hasNextLine()) {
+            var username = scanner.nextLine();
             System.out.println("Hello, " + username);
         } else {
             System.out.println("No input provided!");
         }
+
+        scanner.close();
         System.exit(0);
     }
 }
