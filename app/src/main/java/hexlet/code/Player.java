07bb691602bfee8de2534046658af14a228821c7
Zorgit <piligrimexe@gmail.com>
2023-09-15 21:54:55 +0300

    Create new base class Player to identify user

---
+++ app/src/main/java/hexlet/code/Player.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+class Player {
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
