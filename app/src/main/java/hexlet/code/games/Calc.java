8e89d4e810baec540b7d7f154abd5f49ba5ab771
Zorgit <piligrimexe@gmail.com>
2023-09-17 22:48:23 +0300

    Add Calc game code

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,5 +1,38 @@
 package hexlet.code.games;
 
-public class Calc {
+import hexlet.code.Engine;
+import hexlet.code.Player;
+import hexlet.code.Util;
 
+public class Calc {
+    public static void start(Player player) {
+        Engine engine = new Engine(player);
+        System.out.println("What is the result of the expression?");
+
+        while (engine.getCorrectAnswerCounter() < 3) {
+            String question = null;
+            var num1 = Util.getRandomPositiveInt(5);
+            var num2 = Util.getRandomPositiveInt(30);
+            var operator = Util.getRandomPositiveInt(3);
+            var correctAnswer = 0;
+            switch (operator) {
+                case 2:
+                    question = num1 + " - " + num2;
+                    correctAnswer = num1 - num2;
+                    break;
+                case 3:
+                    question = num1 + " * " + num2;
+                    correctAnswer = num1 * num2;
+                    break;
+                default:
+                    question = num1 + " + " + num2;
+                    correctAnswer = num1 + num2;
+                    break;
+            }
+            engine.showQuestion(question);
+            var answer = engine.getAnswer();
+            engine.checkResult(player, answer, String.valueOf(correctAnswer));
+        }
+        System.out.println("Congratulations, " + player.getName() + "!");
+    }
 }
