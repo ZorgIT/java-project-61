90dc0aa5b983138bd5f1743f6cb203a2457ffd43
Zorgit <piligrimexe@gmail.com>
2023-09-17 22:47:24 +0300

    Create Engile class with main game method

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,44 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    private int correctAnswerCounter;
+
+    public Engine(Player player) {
+        correctAnswerCounter = 0;
+        Cli.start(player);
+    }
+    public void showQuestion(String question) {
+        System.out.println("Question: " + question);
+    }
+    public String getAnswer() {
+        System.out.println("Your answer: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+    public void checkResult(Player player, String playerAnswer,
+                                       String correctAnswer) {
+        if (playerAnswer.toLowerCase().equals(correctAnswer)) {
+            System.out.println("Correct!" + "\n");
+            correctAnswerCounterInc();
+        } else {
+            System.out.println("`" + playerAnswer + "` is wrong answer ;(. "
+                    + "Correct answer was `" + correctAnswer + "`");
+            System.out.println("Let's try again, " + player.getName()
+                    + "!" + "\n");
+            correctAnswerCounterReset();
+        }
+    }
+    public int getCorrectAnswerCounter() {
+        return correctAnswerCounter;
+    }
+
+    public void correctAnswerCounterInc() {
+        this.correctAnswerCounter += 1;
+    }
+
+    public void correctAnswerCounterReset() {
+        this.correctAnswerCounter = 0;
+    }
+}
