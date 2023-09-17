e1604383e25850f91ae74cd890383fc13564c227
Zorgit <piligrimexe@gmail.com>
2023-09-17 23:09:16 +0300

    Add GCD function

--- app/src/main/java/hexlet/code/Util.java
+++ app/src/main/java/hexlet/code/Util.java
@@ -9,4 +9,12 @@ public class Util {
         var randomNumber = random.nextInt(upperBound) + 1;
         return randomNumber;
     }
+
+    public static int getGCD(int n1, int n2) {
+        if (n2 == 0) {
+            return n1;
+        }
+        return getGCD(n2, n1 % n2);
+    }
+
 }
