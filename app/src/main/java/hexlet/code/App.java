97a2107c7461097f54c045b28793af52d09f9b11
Zorgit <piligrimexe@gmail.com>
2023-09-17 23:10:43 +0300

    Expand select menu to 4 game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ public class App {
             System.out.println("1 - Greet");
             System.out.println("2 - Even");
             System.out.println("3 - Calc");
+            System.out.println("4 - GCD");
             System.out.println("0 - exit");
             System.out.print("Your choose: ");
             Scanner scanner = new Scanner(System.in);
@@ -28,6 +30,9 @@ public class App {
                     break;
                 case 3:
                     Calc.start(player);
+                    break;
+                case 4:
+                    GCD.start(player);
                     break;
                 case 0:
                     resumeGame = false;
