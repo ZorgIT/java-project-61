c35dbd3738a8df43c55e99fd6491b3fd9e345b10
Zorgit <piligrimexe@gmail.com>
2023-09-19 23:48:44 +0300

    Add Prime Game

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,33 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import hexlet.code.Player;
+import hexlet.code.Util;
+
+import java.util.Arrays;
+
+public class Prime {
+    public static void  start(Player player) {
+        Engine engine = new Engine(player);
+        System.out.println("Answer 'yes' if given number is prime. "
+                + "Otherwise answer 'no'." + "\n");
+        int[] simpleNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+                101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157,
+                163, 167, 173, 179, 181, 191, 193, 197, 199};
+
+        while (engine.getCorrectAnswerCounter() < 3) {
+            var questionNum = Util.getRandomPositiveInt(200);
+            engine.showQuestion(String.valueOf(questionNum));
+            var answer = engine.getAnswer();
+            var correctAnswer =
+                    (Arrays.binarySearch(simpleNumbers, questionNum) > -1)
+                            ? "yes" : "no";
+            engine.checkResult(player, answer, correctAnswer);
+            System.out.println("test " + Arrays.binarySearch(simpleNumbers, +
+                    questionNum));
+        }
+        System.out.println("Congratulations, " + player.getName() + "!");
+    }
+
+}
